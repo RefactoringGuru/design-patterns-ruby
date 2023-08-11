07f24ae01d6ac18c832c44e31f6eb6113a6ef6e8
Victor Palombo Silvano <victorpsilvano@gmail.com>
2023-08-11 14:48:11 +0100

    style: The variable "result" was redundant https://www.rubydoc.info/gems/rubocop/RuboCop/Cop/Style/RedundantAssignment

--- src/factory_method/conceptual/main.rb
+++ src/factory_method/conceptual/main.rb
@@ -53,9 +53,7 @@ class Creator
     # EN: Now, use the product.
     #
     # RU: Далее, работаем с этим продуктом.
-    result = "Creator: The same creator's code has just worked with #{product.operation}"
-
-    result
+    "Creator: The same creator's code has just worked with #{product.operation}"
   end
 end
 
